9efc0479abdfd91e4ed9b7945f6b3c427d129aee
Kangmingu <kangmin1012>
2020-08-04 15:58:42 +0900

    코루틴 / 컬러 / 폰트 세팅

--- app/src/main/java/org/three/minutes/MainActivity.kt
+++ app/src/main/java/org/three/minutes/MainActivity.kt
@@ -2,10 +2,7 @@ package org.three.minutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProviders
-import kotlinx.android.synthetic.main.activity_main.*
 import org.three.minutes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
