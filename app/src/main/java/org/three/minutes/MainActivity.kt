a1348b5444410d49e06f795afcfb31e161caeb23
Kangmingu <kangmin1012>
2020-08-04 17:15:05 +0900

    폰트 스타일 추가 + 로그인 버튼까지 xml 완료

--- app/src/main/java/org/three/minutes/MainActivity.kt
+++ app/src/main/java/org/three/minutes/MainActivity.kt
@@ -1,17 +1,39 @@
 package org.three.minutes
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.activity_main.*
 import org.three.minutes.databinding.ActivityMainBinding
+import org.three.minutes.singleton.StatusObject
+import org.three.minutes.util.keyBoardObserve
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var mImm : InputMethodManager
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding  = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this
 
+        mImm = getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as android.view.inputmethod.InputMethodManager
+
+        //상태바 투명으로 만들기
+        StatusObject.setStatusBar(this)
+
+        this.keyBoardObserve {
+            login_edt.clearFocus()
+            password_edt.clearFocus()
+        }
+
+
+    }
+
+    fun hideKeyboard() {
+        mImm.hideSoftInputFromWindow(login_edt.windowToken, 0)
     }
 }
