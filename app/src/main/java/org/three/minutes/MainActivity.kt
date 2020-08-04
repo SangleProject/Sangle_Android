3784fa1c5096c2f96724e2d4a3a7990eec59dfdf
Kangmingu <kangmin1012>
2020-08-04 17:51:53 +0900

    로그인 화면 구성 끝

--- app/src/main/java/org/three/minutes/MainActivity.kt
+++ app/src/main/java/org/three/minutes/MainActivity.kt
@@ -1,9 +1,9 @@
 package org.three.minutes
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import org.three.minutes.databinding.ActivityMainBinding
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         //상태바 투명으로 만들기
         StatusObject.setStatusBar(this)
 
+        //키보드가 내려가면 포커스 해제
         this.keyBoardObserve {
             login_edt.clearFocus()
             password_edt.clearFocus()
@@ -33,7 +34,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // 레이아웃 클릭 시 키보드 내리기
     fun hideKeyboard() {
-        mImm.hideSoftInputFromWindow(login_edt.windowToken, 0)
+        if(mImm.isAcceptingText){
+            mImm.hideSoftInputFromWindow(this.currentFocus?.windowToken, 0)
+        }
+
+    }
+
+    fun checkLogin(){
+        Toast.makeText(this,"login",Toast.LENGTH_SHORT).show()
     }
 }
