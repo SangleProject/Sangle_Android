ac8329a849c8bb0cbfef073604a69fee9c203d23
Kangmingu <kangmin1012>
2020-08-11 14:18:13 +0900

    약관 관련 문장 작성

---
+++ app/src/main/java/org/three/minutes/signup/ui/claus/ClausActivity.kt
@@ -0,0 +1,12 @@
+package org.three.minutes.signup.ui.claus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.three.minutes.R
+
+class ClausActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_claus)
+    }
+}
