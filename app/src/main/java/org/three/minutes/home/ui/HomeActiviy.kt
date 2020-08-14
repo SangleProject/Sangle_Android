5babee4f373060e9727164c42d91ea649af56db7
Kangmingu <kangmin1012>
2020-08-14 23:11:59 +0900

    홈 화면 기본 레이아웃 준비

---
+++ app/src/main/java/org/three/minutes/home/ui/HomeActiviy.kt
@@ -0,0 +1,13 @@
+package org.three.minutes.home.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.three.minutes.R
+
+class HomeActiviy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
