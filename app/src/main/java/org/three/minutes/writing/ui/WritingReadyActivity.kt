b4cdba713c54bf15951dc479525e68537dd19a70
Kangmingu <kangmin1012>
2020-10-11 22:57:20 +0900

    WritingReadyActivity 완료

--- app/src/main/java/org/three/minutes/writing/ui/WritingReadyActivity.kt
+++ app/src/main/java/org/three/minutes/writing/ui/WritingReadyActivity.kt
@@ -3,25 +3,28 @@ package org.three.minutes.writing.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_writing_ready.*
+import androidx.databinding.DataBindingUtil
 import kotlinx.coroutines.*
 import org.three.minutes.R
+import org.three.minutes.databinding.ActivityWritingReadyBinding
 import kotlin.coroutines.CoroutineContext
 
 class WritingReadyActivity : AppCompatActivity(), CoroutineScope {
     private lateinit var job: Job
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
+    private val mBinding : ActivityWritingReadyBinding by lazy{
+        DataBindingUtil.setContentView(this,R.layout.activity_writing_ready)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_writing_ready)
-        job = Job()
-
-        ready_lottie.apply {
-            setAnimation("keyword_aos.json")
-
+        mBinding.apply {
+            lifecycleOwner = this@WritingReadyActivity
+            activity = this@WritingReadyActivity
         }
+        job = Job()
+        
         launch {
             delay(3000)
 
@@ -36,5 +39,9 @@ class WritingReadyActivity : AppCompatActivity(), CoroutineScope {
     override fun onDestroy() {
         super.onDestroy()
         job.cancel()
+    }
+
+    fun finishActivity(){
+        finish()
     }
 }
