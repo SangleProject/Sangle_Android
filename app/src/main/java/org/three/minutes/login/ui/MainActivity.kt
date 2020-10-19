b164c819f10e9c743b7bdd98715312740d3beb63
Kangmingu <kangmin1012>
2020-10-19 18:46:25 +0900

    Google Login 구현

--- app/src/main/java/org/three/minutes/login/ui/MainActivity.kt
+++ app/src/main/java/org/three/minutes/login/ui/MainActivity.kt
@@ -7,20 +7,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import org.three.minutes.R
 import org.three.minutes.databinding.ActivityMainBinding
 import org.three.minutes.home.ui.HomeActiviy
 import org.three.minutes.signup.ui.SignupActivity
 import org.three.minutes.singleton.StatusObject
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mImm: InputMethodManager
+    private val gso : GoogleSignInOptions by lazy {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+    }
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
+
+    private val GOOGLE_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -33,7 +50,34 @@ class MainActivity : AppCompatActivity() {
         //상태바 투명으로 만들기
         StatusObject.setStatusBar(this)
 
+        // 파이어베이스 인증객체 초기화 + 클라이언트 가져오기
+        mAuth = FirebaseAuth.getInstance()
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
 
+
+        // 구글 로그인 버튼 클릭 시 구글 로그인 연동
+        binding.signinGoogle.setOnClickListener {
+            googleSignIn()
+        }
+
+        // 구글 로그아웃
+        binding.signoutGoogle.setOnClickListener {
+            googleSignOut()
+        }
+
+    }
+
+    private fun googleSignOut() {
+        mAuth.signOut()
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener {
+            Toast.makeText(this,"SignOutSuccess",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // 구글 로그인 intent 객체 생성 후 전달
+    private fun googleSignIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_CODE)
     }
 
     // 레이아웃 클릭 시 키보드 내리기
@@ -63,6 +107,38 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
         ActivityCompat.finishAffinity(this)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // 구글 로그인 인증 확인
+        if (requestCode == GOOGLE_CODE){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("GoogleInfo","id : ${account.id} , idToken : ${account.idToken}")
+                Log.d("GoogleInfo","email : ${account.email} , displayName : ${account.displayName}")
+                firebaseAuthWithGoogle(account.idToken!!)
+            }
+            catch (e : ApiException){
+
+            }
+        }
+    }
+
+    // 인증에 성공했을 때 실제로 로그인이 되었는지?
+    private fun firebaseAuthWithGoogle( idToken : String) {
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful){
+                    Toast.makeText(this,"GoogleLoginOk",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this,"GoogleLoginFail",Toast.LENGTH_SHORT).show()
+                }
+            }
 
     }
 }
