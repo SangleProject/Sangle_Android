a39adfb0fcb5e9ad3542d57d64301919c24584d3
Kangmingu <kangmin1012>
2021-01-03 00:35:49 +0900

    [WritingDetail] 삭제하기 클릭 이벤트 + task 관리

--- app/src/main/java/org/three/minutes/writing/ui/WritingResultActivity.kt
+++ app/src/main/java/org/three/minutes/writing/ui/WritingResultActivity.kt
@@ -1,5 +1,6 @@
 package org.three.minutes.writing.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
@@ -8,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import kotlinx.android.synthetic.main.activity_writing_result.*
 import org.three.minutes.R
 import org.three.minutes.databinding.ActivityWritingResultBinding
+import org.three.minutes.home.ui.HomeActivity
 import org.three.minutes.writing.viewmodel.WritingResultViewModel
 
 class WritingResultActivity : AppCompatActivity() {
@@ -28,11 +30,32 @@ class WritingResultActivity : AppCompatActivity() {
         setSupportActionBar(result_toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        val intent = intent
         mViewModel.contents.value = intent.getStringExtra("contents")
         mViewModel.topic.value = intent.getStringExtra("topic")
 
         // 글 내용 TextView 스크롤
         mBinding.resultContentsTxt.movementMethod = ScrollingMovementMethod()
+
+        // 좌측 상단 완료 버튼
+        mBinding.resultDone.setOnClickListener {
+            startHomeActivity()
+        }
+        // 삭제하기 버튼
+        mBinding.resultDeleteTxt.setOnClickListener {
+            startHomeActivity()
+        }
+
+    }
+
+    // 완료 버튼, 삭제 버튼, 뒤로가기 버튼 클릭 시 호출
+    private fun startHomeActivity(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finishAndRemoveTask()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startHomeActivity()
     }
 }
