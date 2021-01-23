d40f4590c96384a44a125a2d911d537b5220292f
Kangmingu <kangmin1012>
2021-01-24 00:46:29 +0900

    [ADD] #119 설정 알림 구성

---
+++ app/src/main/java/org/three/minutes/preferences/ui/PreferencesActivity.kt
@@ -0,0 +1,27 @@
+package org.three.minutes.preferences.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.three.minutes.R
+import org.three.minutes.databinding.ActivityPreferencesBinding
+import org.three.minutes.singleton.StatusObject
+import javax.net.ssl.SSLEngineResult
+
+class PreferencesActivity : AppCompatActivity() {
+    private val mBinding : ActivityPreferencesBinding by lazy{
+        DataBindingUtil.setContentView(this,R.layout.activity_preferences)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        StatusObject.setStatusBar(this)
+
+        mBinding.apply {
+            lifecycleOwner = this@PreferencesActivity
+        }
+
+        mBinding.notificationAllSwitch.isEnabled = false
+
+    }
+}
