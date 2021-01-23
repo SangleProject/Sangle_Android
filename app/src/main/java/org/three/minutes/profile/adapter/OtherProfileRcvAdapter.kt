4f0d4d772f3175ffe25be718852b585a99f486f5
Kangmingu <kangmin1012>
2021-01-23 16:20:28 +0900

    [ADD] #117 Rcv Adapter 생성

---
+++ app/src/main/java/org/three/minutes/profile/adapter/OtherProfileRcvAdapter.kt
@@ -0,0 +1,53 @@
+package org.three.minutes.profile.adapter
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.RecyclerView
+import org.three.minutes.R
+import org.three.minutes.databinding.MyWritingItemListBinding
+import org.three.minutes.detail.data.ResponseOtherWritingData
+import org.three.minutes.detail.ui.DetailActivity
+import org.three.minutes.util.formatCount
+
+class OtherProfileRcvAdapter
+    : RecyclerView.Adapter<OtherProfileRcvAdapter.OtherProfileViewHolder>() {
+
+    val data = listOf<ResponseOtherWritingData>()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OtherProfileViewHolder {
+        val binding = MyWritingItemListBinding
+            .inflate(LayoutInflater.from(parent.context),parent,false)
+        return OtherProfileViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: OtherProfileViewHolder, position: Int) {
+        holder.onBind(data[position])
+    }
+
+
+    inner class OtherProfileViewHolder(val binding : MyWritingItemListBinding)
+        : RecyclerView.ViewHolder(binding.root){
+
+        @SuppressLint("SetTextI18n")
+        fun onBind(data : ResponseOtherWritingData){
+            binding.container.animation = AnimationUtils.loadAnimation(itemView.context, R.anim.item_scale_animation)
+
+            binding.itemTitleTxt.text = data.topic
+            binding.itemDateTxt.text = "${data.date} (${data.day} ${data.time})"
+            binding.itemContents.text = data.postWrite
+            binding.itemFavorite.text = data.likes.formatCount()
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, DetailActivity::class.java)
+                intent.putExtra("postIdx",data.postIdx)
+                itemView.context.startActivity(intent)
+            }
+        }
+    }
+}
