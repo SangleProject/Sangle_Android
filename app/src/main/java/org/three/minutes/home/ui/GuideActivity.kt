1ae02ddb8c8e487a545244adeacd620afd18cebb
Kangmingu <kangmin1012>
2021-01-26 00:16:58 +0900

    [ADD] #121 이용안내 뷰 사전 준비

---
+++ app/src/main/java/org/three/minutes/home/ui/GuideActivity.kt
@@ -0,0 +1,21 @@
+package org.three.minutes.home.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.three.minutes.R
+import org.three.minutes.databinding.ActivityGuideBinding
+
+class GuideActivity : AppCompatActivity() {
+    private val mBinding : ActivityGuideBinding by lazy{
+        DataBindingUtil.setContentView(this,R.layout.activity_guide)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding.apply {
+            lifecycleOwner = this@GuideActivity
+        }
+
+
+    }
+}
