b0fa265f2e79978d9df8418afc5240fe8ed1a478
Kangmingu <kangmin1012>
2021-01-26 21:42:02 +0900

    [ADD] #123 온보딩 전부 완료
    4번 온보딩 이미지 새로 받으면 수정할 것

--- app/src/main/java/org/three/minutes/login/ui/OnBoardingFragment6.kt
+++ app/src/main/java/org/three/minutes/login/ui/OnBoardingFragment6.kt
@@ -22,4 +22,8 @@ class OnBoardingFragment6 : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 }
