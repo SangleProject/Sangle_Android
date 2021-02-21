582085667d5cd5433c9b8541fc2944640f40b39c
kangmingu <kmg@paytalab.com>
2021-02-21 15:52:31 +0900

    온보딩 화면 이동 수정 + 글감 뷰 수정

--- app/src/main/java/org/three/minutes/word/ui/WordFragment.kt
+++ app/src/main/java/org/three/minutes/word/ui/WordFragment.kt
@@ -120,50 +120,5 @@ class WordFragment : Fragment() {
             }
         }
 
-        setFilterObserving()
-        setFilterClickListener()
-    }
-
-    private fun setFilterClickListener() {
-        mBinding.pastWritingFilter.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                R.id.past_writing_all -> mViewModel.allCheck.value = true
-                R.id.past_writing_done -> mViewModel.doneCheck.value = true
-                R.id.past_writing_not -> mViewModel.notCheck.value = true
-            }
-        }
-    }
-
-    private fun setFilterObserving() {
-
-        mViewModel.allCheck.observe(viewLifecycleOwner, { check ->
-            if (check) {
-                mBinding.pastWritingAll.typeface = Typeface.DEFAULT_BOLD
-                mViewModel.doneCheck.value = false
-                mViewModel.notCheck.value = false
-            } else {
-                mBinding.pastWritingAll.typeface = Typeface.DEFAULT
-            }
-        })
-
-        mViewModel.doneCheck.observe(viewLifecycleOwner, { check ->
-            if (check) {
-                mBinding.pastWritingDone.typeface = Typeface.DEFAULT_BOLD
-                mViewModel.allCheck.value = false
-                mViewModel.notCheck.value = false
-            } else {
-                mBinding.pastWritingDone.typeface = Typeface.DEFAULT
-            }
-        })
-
-        mViewModel.notCheck.observe(viewLifecycleOwner, { check ->
-            if (check) {
-                mBinding.pastWritingNot.typeface = Typeface.DEFAULT_BOLD
-                mViewModel.allCheck.value = false
-                mViewModel.doneCheck.value = false
-            } else {
-                mBinding.pastWritingNot.typeface = Typeface.DEFAULT
-            }
-        })
     }
 }
