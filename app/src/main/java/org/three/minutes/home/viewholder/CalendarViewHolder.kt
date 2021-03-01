7f635ba329795cf6fc64930d0c247ecdc2b9899b
kangmingu <kangmin1012@naver.com>
2021-03-01 16:07:23 +0900

    캘린더 디자인 위치 수정

--- app/src/main/java/org/three/minutes/home/viewholder/CalendarViewHolder.kt
+++ app/src/main/java/org/three/minutes/home/viewholder/CalendarViewHolder.kt
@@ -1,6 +1,7 @@
 package org.three.minutes.home.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.three.minutes.R
 import org.three.minutes.databinding.CalendarDayItemBinding
 import org.three.minutes.home.data.CalendarData
@@ -11,13 +12,14 @@ class CalendarViewHolder (val binding : CalendarDayItemBinding) : RecyclerView.V
         binding.calendarData = data
         when(data.count){
             1->{
-                binding.calendarDay.setBackgroundResource(R.drawable.circle_light_mint)
+                Glide.with(itemView).load(R.drawable.circle_light_mint).into(binding.calendarCircle)
             }
             2 ->{
-                binding.calendarDay.setBackgroundResource(R.drawable.circle_sky_blue)
+                Glide.with(itemView).load(R.drawable.circle_sky_blue).into(binding.calendarCircle)
             }
             3 ->{
-                binding.calendarDay.setBackgroundResource(R.drawable.circle_deep_sky)
+                Glide.with(itemView).load(R.drawable.circle_deep_sky).into(binding.calendarCircle)
+
             }
         }
     }
