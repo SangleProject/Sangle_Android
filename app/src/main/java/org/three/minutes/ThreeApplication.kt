967b522a395bf3ab66d6797937fb7f9657e88e07
Kangmingu <kangmin1012>
2021-03-13 22:55:12 +0900

    알림 토픽 구독

--- app/src/main/java/org/three/minutes/ThreeApplication.kt
+++ app/src/main/java/org/three/minutes/ThreeApplication.kt
@@ -2,10 +2,15 @@ package org.three.minutes
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import org.three.minutes.util.*
 
 class ThreeApplication : Application(){
@@ -22,7 +27,27 @@ class ThreeApplication : Application(){
         threeApplication = this
         mImm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         dataStore = SangleDataStoreManager(this)
+
         FirebaseMessaging.getInstance().subscribeToTopic("test")
+        CoroutineScope(Dispatchers.IO).launch {
+
+            val notice = dataStore.isNotification.first()
+            val motive = dataStore.isMotive.first()
+
+            if (notice){
+                FirebaseMessaging.getInstance().subscribeToTopic("notice")
+            }
+            else{
+                FirebaseMessaging.getInstance().unsubscribeFromTopic("notice")
+            }
+
+            if (motive){
+                FirebaseMessaging.getInstance().subscribeToTopic("motive")
+            }
+            else{
+                FirebaseMessaging.getInstance().unsubscribeFromTopic("motive")
+            }
+        }
     }
 
     fun getInputMethodManager() : InputMethodManager = mImm
