1ca114d3cbc30cd28448de06d8ed804620240f4a
Kang min gu <55642709+kangmin1012@users.noreply.github.com>
2021-12-30 21:33:01 +0900

    master 1.3.0
    * 지난 글감 리스트 인기순 디폴트
    
    * 글감 미리보기 크기 유동적으로 변경
    
    * 메인 화면 리뉴얼
    
    * 생글 인스타 페이지 연결
    
    * 1.2.0 version up
    
    * 글감 화면 라이팅 변경
    
    * buildCode up
    
    202109081
    
    * 메인 페이지 프로필 위치조정
    
    * 1.3.0 업데이트 적용
    
    * base URL 변경
    
    * 강제 업데이트시 안내 추가
    
    * 공지 알림 스위치 삭제 + 동기부여 알림 설명 추가
    
    * 홈 페이지 스와이프 disable
    
    * version code up 1.3.0

--- app/src/main/java/org/three/minutes/login/ui/SplashActivity.kt
+++ app/src/main/java/org/three/minutes/login/ui/SplashActivity.kt
@@ -1,6 +1,8 @@
 package org.three.minutes.login.ui
 
+import android.app.Dialog
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +11,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.asLiveData
 import com.airbnb.lottie.LottieDrawable
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.coroutines.*
@@ -22,6 +25,8 @@ import org.three.minutes.home.ui.HomeActivity
 import org.three.minutes.server.SangleServiceImpl
 import org.three.minutes.signup.data.ResponseSignUpData
 import org.three.minutes.singleton.StatusObject
+import org.three.minutes.util.AppVersionChecker
+import org.three.minutes.util.CustomDialog
 import org.three.minutes.util.customEnqueue
 import org.three.minutes.util.showToast
 import retrofit2.Call
@@ -32,11 +37,24 @@ class SplashActivity : AppCompatActivity() {
     private var refresh = ""
     private var onBoarding = true
 
+    private val enforceDialog by lazy {
+        CustomDialog(
+            context = this,
+            title = "업데이트 알림",
+            content = "정상적인 앱 사용을 위해\n업데이트가 필요합니다.\n확인을 누르면 스토어로 이동합니다.",
+            cancelTitle = "취소",
+            okTitle = "확인",
+            dialogImg = R.drawable.illust_popup02,
+            isCancelable = false
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         StatusObject.setStatusBar(this)
+        initDialog()
 
 //         fcm을 위한 현재 기기 토큰 받기
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
@@ -53,14 +71,65 @@ class SplashActivity : AppCompatActivity() {
         })
 
         CoroutineScope(Main).launch {
+            onBoarding = ThreeApplication.getInstance().getDataStore().isOnBoarding.first()
+            splash_img.setAnimation("splash.json")
+            splash_img.repeatCount = LottieDrawable.INFINITE
+            delay(2000)
+        }
 
-            launch {
-                onBoarding = ThreeApplication.getInstance().getDataStore().isOnBoarding.first()
-                splash_img.setAnimation("splash.json")
-                splash_img.repeatCount = LottieDrawable.INFINITE
-                delay(2000)
-            }.join()
+        ThreeApplication.getInstance().getFireStore()
+            .collection("updates")
+            .document("enforceUpdate")
+            .get()
+            .addOnSuccessListener { document ->
+                checkEnforceUpdate(document)
+            }.addOnFailureListener {
+                instanceData()
+            }
 
+
+
+
+    }
+
+    private fun initDialog() {
+        enforceDialog.setDialogClickListener(object : CustomDialog.ClickListener {
+            override fun setOnOk(dialog: Dialog) {
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse("https://play.google.com/store/apps/details?id=${packageName}")
+                }
+                startActivity(intent)
+                moveTaskToBack(true); // 태스크를 백그라운드로 이동
+                finishAndRemoveTask(); // 액티비티 종료 + 태스크 리스트에서 지우기
+            }
+
+            override fun setOnCancel(dialog: Dialog) {
+                moveTaskToBack(true); // 태스크를 백그라운드로 이동
+                finishAndRemoveTask(); // 액티비티 종료 + 태스크 리스트에서 지우기
+            }
+        })
+    }
+
+    private fun checkEnforceUpdate(document: DocumentSnapshot?) {
+        if (document == null || document["android_version"] == null) {
+            instanceData()
+            return
+        }
+
+        val deviceUpdate = packageManager.getPackageInfo(packageName, 0).versionName.replace(".","").toInt()
+        val updateVersion = document["android_version"].toString().
+            replace(".","").toInt()
+
+        if (updateVersion - deviceUpdate >= 10) {
+            enforceDialog.show()
+        } else {
+            instanceData()
+        }
+
+    }
+
+    private fun instanceData() {
+        CoroutineScope(Main).launch {
             if (onBoarding) {
                 ThreeApplication.getInstance().getDataStore().getDataStoreCore().edit {
                     it.clear()
@@ -74,8 +143,6 @@ class SplashActivity : AppCompatActivity() {
                 checkAutoLogin()
             }
         }
-
-
     }
 
     private fun checkAutoLogin() {
